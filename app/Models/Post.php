f7ea654b64c88873730865feb57687914ca21ab4
berlianaputri8 <75973686+berlianaputri8@users.noreply.github.com>
2022-10-25 16:14:21 +0700

    Menyelesaikan Category & Relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,8 @@ class Post extends Model
     // protected $fillable = ['tittle', 'excerpt', 'body'];
     protected $guarded = ['id'];
     
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,14 @@
 
 @section('container')
     <article>
-        <h2>{{ $post["tittle"] }}</h2>
+        <h1>{{ $post->tittle }}</h1>
+
+        <p>By. Berliana Putri in 
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </p>
+
         {!! $post->body !!}
+
     </article>
     
     <a href="/posts">Back to Post</a>

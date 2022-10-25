be112ea11869055988c0abb233126fe24339387e
berlianaputri8 <75973686+berlianaputri8@users.noreply.github.com>
2022-10-25 17:20:05 +0700

    Menyelesaikan Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Berliana Putri',
+            'email' => 'berlianaap@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Qisti Izatussyabani',
+            'email' => 'qisitiiz@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'tittle' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos. Vitae earum blanditiis debitis quisquam voluptas sint distinctio sapiente laborum temporibus, quasi veniam nulla ad, aperiam officiis ratione dolorum praesentium minima vero eligendi quidem tempore et. Voluptate praesentium quidem veritatis animi possimus culpa dolores beatae, doloribus, rem neque cum nobis ipsum? Officiis numquam suscipit voluptatem fugiat dolores fuga dicta alias excepturi voluptatum nihil dolorem quaerat quos impedit voluptates eveniet blanditiis expedita, aspernatur quo animi. Fugit, eveniet!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'tittle' => 'Judul Kedua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos. Vitae earum blanditiis debitis quisquam voluptas sint distinctio sapiente laborum temporibus, quasi veniam nulla ad, aperiam officiis ratione dolorum praesentium minima vero eligendi quidem tempore et. Voluptate praesentium quidem veritatis animi possimus culpa dolores beatae, doloribus, rem neque cum nobis ipsum? Officiis numquam suscipit voluptatem fugiat dolores fuga dicta alias excepturi voluptatum nihil dolorem quaerat quos impedit voluptates eveniet blanditiis expedita, aspernatur quo animi. Fugit, eveniet!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'tittle' => 'Judul Tiga',
+            'slug' => 'judul-ke-tida',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos. Vitae earum blanditiis debitis quisquam voluptas sint distinctio sapiente laborum temporibus, quasi veniam nulla ad, aperiam officiis ratione dolorum praesentium minima vero eligendi quidem tempore et. Voluptate praesentium quidem veritatis animi possimus culpa dolores beatae, doloribus, rem neque cum nobis ipsum? Officiis numquam suscipit voluptatem fugiat dolores fuga dicta alias excepturi voluptatum nihil dolorem quaerat quos impedit voluptates eveniet blanditiis expedita, aspernatur quo animi. Fugit, eveniet!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'tittle' => 'Judul Keempat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos.',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui enim, nisi blanditiis culpa similique praesentium voluptatum error sit hic ratione cupiditate eveniet et vel accusamus eum nam suscipit quos impedit assumenda iure quaerat eos. Vitae earum blanditiis debitis quisquam voluptas sint distinctio sapiente laborum temporibus, quasi veniam nulla ad, aperiam officiis ratione dolorum praesentium minima vero eligendi quidem tempore et. Voluptate praesentium quidem veritatis animi possimus culpa dolores beatae, doloribus, rem neque cum nobis ipsum? Officiis numquam suscipit voluptatem fugiat dolores fuga dicta alias excepturi voluptatum nihil dolorem quaerat quos impedit voluptates eveniet blanditiis expedita, aspernatur quo animi. Fugit, eveniet!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,13 +4,13 @@
     <article>
         <h1>{{ $post->tittle }}</h1>
 
-        <p>By. Berliana Putri in 
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a>
+            <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
         </p>
 
         {!! $post->body !!}
 
     </article>
     
-    <a href="/posts">Back to Post</a>
+    <a href="/posts" class="d-block mt-3">Back to Post</a>
 @endsection

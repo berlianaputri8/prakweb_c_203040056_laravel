a88807f28fe74861a3e2b48f2d0603837872645c
berlianaputri8 <75973686+berlianaputri8@users.noreply.github.com>
2022-12-03 17:17:40 +0700

    memperbaiki file create dan edit

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -83,17 +83,17 @@
             e.preventDefault();
         })
 
-        function previewImage() {
+        function previewImage() { 
             const image = document.querySelector('#image');
             const imgPreview = document.querySelector('.img-preview');
-
+        
             imgPreview.style.display = 'block';
-
+        
             const oFReader = new FileReader();
             oFReader.readAsDataURL(image.files[0]);
-
-            oFReader.onload = function(oFReader) {
-                imgPreview.src = oFReader.target.result;
+        
+            oFReader.onload = function(oFREvent) {
+                imgPreview.src = oFREvent.target.result;
             }
         }
     </script>
